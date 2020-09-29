f4b6d8390144f15f42b543281fd81118cc45b5a2
ShineySun <ksp2246@naver.com>
2020-09-29 19:02:10 +0900

    complete data_builder.py

--- data_builder.py
+++ data_builder.py
@@ -9,7 +9,10 @@ class Data_Builder_Tusimple:
     def __init__(self, K = 4, data_path = "tusimple"):
         self._K = K
         self._data_path = data_path
-        self._train_json_file_path = os.path.join(self._data_path, r'label_data_0531.json')
+        # train : label_data_0313.json, label_data_0601.json
+        # validation : label_data_0531.json
+        # test : test_label.json
+        self._train_json_file_path = os.path.join(self._data_path, r'label_data_0313.json')
 
     def quick_inspector(self):
         data_list = []
@@ -60,6 +63,88 @@ class Data_Builder_Tusimple:
                 data = json.loads(line)
                 data_list.append(data)
 
+        f_w =  open('test.txt','w')
+        data_name = 0
+
+        for data_entry in tqdm(data_list):
+            clip_dir = os.path.dirname(data_entry['raw_file'])
+
+            number_of_class = len(data_entry['lanes'])
+
+            image_list = list()
+
+            if self._K == number_of_class:
+                bgr_image = cv2.imread(os.path.join(self._data_path, clip_dir, '20.jpg'))
+                #print("os.path.join(self._data_path, clip_dir) : {}".format(os.path.join(self._data_path, clip_dir)))
+
+                seg_image_0 = np.zeros_like(bgr_image)
+                seg_image_1 = np.zeros_like(bgr_image)
+                seg_image_2 = np.zeros_like(bgr_image)
+                seg_image_3 = np.zeros_like(bgr_image)
+
+                lane_counter = 0
+
+
+                for lane_idx, lane_x_points in enumerate(data_entry['lanes'],0):
+                    print("Lane_Index : {}".format(lane_idx))
+                    print("Lane X Points : {}".format(len(np.unique(lane_x_points))))
+
+                    if len(np.unique(lane_x_points)) > 1:
+                        lane_counter += 1
+
+                    curve_vertices = list(filter(lambda xy_pair : xy_pair[0] > 0, zip(lane_x_points, data_entry['h_samples'])))
+
+                    for vertex_1, vertex_2 in zip(curve_vertices[:-1], curve_vertices[1:]):
+                        if lane_idx == 0:
+                            cv2.line(seg_image_0, tuple(vertex_1), tuple(vertex_2), (255,255,255), 5)
+
+                        elif lane_idx == 1:
+                            cv2.line(seg_image_1, tuple(vertex_1), tuple(vertex_2), (255,255,255), 5)
+
+                        elif lane_idx == 2:
+                            cv2.line(seg_image_2, tuple(vertex_1), tuple(vertex_2), (255,255,255), 5)
+
+                        elif lane_idx == 3:
+                            cv2.line(seg_image_3, tuple(vertex_1), tuple(vertex_2), (255,255,255), 5)
+
+                    if lane_idx == 0:
+                        image_list.append(seg_image_0)
+                    if lane_idx == 1:
+                        image_list.append(seg_image_1)
+                    if lane_idx == 2:
+                        image_list.append(seg_image_2)
+                    if lane_idx == 3:
+                        image_list.append(seg_image_3)
+
+                if lane_counter == 4:
+                    for image in image_list:
+                        tmp_name = 'data/class4/' + str(data_name).zfill(4) + '.jpg'
+                        cv2.imwrite(tmp_name, image)
+                            #cv2.imshow("seg_image!!", image)
+                            #cv2.waitKey(-1)
+
+                        tmp_content = str(data_name%4) + ' ' + str(data_name).zfill(4) + '.jpg\n'
+                        f_w.write(tmp_content)
+                        data_name += 1
+
+
+                    print("Lane Counter : {}".format(lane_counter))
+
+                    # cv2.imshow("seg_image_0",seg_image_0)
+                    #
+                    # cv2.imshow("seg_image_1",seg_image_1)
+                    #
+                    # cv2.imshow("seg_image_2",seg_image_2)
+                    #
+                    # cv2.imshow("seg_image_3",seg_image_3)
+                        #cv2.waitKey(1)
+
+
+
+                # cv2.imshow("K CLASS", bgr_image)
+                # cv2.waitKey(-1)
+
+
 
 
 
@@ -75,9 +160,9 @@ def parse_args():
 
 if __name__ == '__main__':
     args = parse_args()
-    data_builder = Data_Builder_Tusimple(K=4, data_path = args.rootDir)
+    data_builder = Data_Builder_Tusimple(K=args.K, data_path = args.rootDir)
 
     if args.mode == 0:
         data_builder.quick_inspector()
     elif args.mode == 1:
-        data_builder.K_Parser(args.K)
+        data_builder.K_Parser()
